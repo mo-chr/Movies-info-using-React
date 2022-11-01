ed96100a7b31cd7b8756b3c410906f5841b7d7bd
Mohammad Chreif <cmohamadch@gmail.com>
2022-11-01 16:19:18 +0200

    Adeed no cover image and fixed favicon

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -11,7 +11,7 @@ const MovieCard = ({ movie }) => {
           src={
             movie.Poster !== `N/A`
               ? movie.Poster
-              : "https://via.placeholder.com/400"
+              : "https://via.placeholder.com/400x400?text=No+Cover"
           }
           alt="Poster"
         ></img>
